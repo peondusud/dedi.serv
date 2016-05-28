8e8d61e873b8520d07820b18300fb0c818810d58
peondusud <reflexion_31@hotmail.com>
2016-05-28 23:58:43 +0200

    Update generate.ssh.key.sh

--- generate.ssh.key.sh
+++ generate.ssh.key.sh
@@ -7,3 +7,6 @@ ssh-keygen -b 521 -t ecdsa
 
 # Verify 
 cat ${HOME}/.ssh/id_{ed25519,rsa,ecdsa}.pub
+
+# Once you have generated a key pair, you will need to copy the public key to the remote server so that it will use SSH key authentication. 
+ssh-copy-id -i ~/.ssh/id_{ed25519,rsa,ecdsa}.pub -p 2222 username@remote-server.org
