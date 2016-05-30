59a20a1f67b59ae4c5fcd699bad8010e0816560e
peondusud <reflexion_31@hotmail.com>
2016-05-30 23:42:07 +0200

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -162,6 +162,18 @@ nft list ruleset
 ## check netfilter log config
 #cat /proc/net/netfilter/nf_log  
 ## 2=IPv4, 4=Novell IPX, 10=IPv6, ...
+#define AF_UNSPEC	  0
+#define AF_UNIX		  1	/* Unix domain sockets 		*/
+#define AF_INET		  2	/* Internet IP Protocol 	*/
+#define AF_AX25		  3	/* Amateur Radio AX.25 		*/
+#define AF_IPX		  4	/* Novell IPX 			*/
+#define AF_APPLETALK  5	/* Appletalk DDP 		*/
+#define AF_NETROM	  6	/* Amateur radio NetROM 	*/
+#define AF_BRIDGE     7	/* Multiprotocol bridge 	*/
+#define AF_AAL5		  8	/* Reserved for Werner's ATM 	*/
+#define AF_X25		  9	/* Reserved for X.25 project 	*/
+#define AF_INET6     10	/* IP version 6			*/
+#define AF_MAX       12	/* For now.. */
 
 # use ipt_LOG for IPv4
 #echo "ipt_LOG" > /proc/sys/net/netfilter/nf_log/2
