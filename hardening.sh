3e7e0e9d48e43f908be87a9e8b6606c5ab27a27b
peondusud <reflexion_31@hotmail.com>
2016-10-18 23:17:35 +0200

    Create hardening.sh

---
+++ hardening.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+fail2ban () {
+      # based on https://wiki.meurisse.org/wiki/Fail2Ban
+      wget -O- http://neuro.debian.net/lists/jessie.de-m.libre > /etc/apt/sources.list.d/neurodebian.sources.list
+      apt-key adv --recv-keys --keyserver hkp://pgp.mit.edu:80 0xA5D32F012649A5A9
+      apt-get update
+      
+      echo "popularity-contest      popularity-contest/participate  boolean false"|  debconf-set-selections
+      #echo "popularity-contest      popularity-contest/submiturls   string"|  debconf-set-selections
+      apt-get install --no-install-recommends --no-install-suggests -y fail2ban python-pyinotify rsyslog whois
+
+      wget https://github.com/peondusud/dedi.serv/blob/master/fail2ban/jail.local -O /etc/fail2ban/jail.local
+      #mv fail2ban/jail.local /etc/fail2ban/jail.local
+
+      wget https://github.com/peondusud/dedi.serv/blob/master/fail2ban/jail.d/recidive.conf -O /etc/fail2ban/jail.d/recidive.conf
+      #mv fail2ban/jail.d/recidive.conf /etc/fail2ban/jail.d/recidive.conf
+
+      sed -i "s|\(port *=\) ssh|\1 ${SSH_PORT}|" /etc/fail2ban/jail.local
+      systemctl start fail2ban
+      systemctl enable fail2ban
+}
+
+portsentry () {
+	apt-get install portsentry
+	
+	sed -i 's|"tcp|"atcp|g' /etc/default/portsentry
+	sed -i 's|"udp|"audp|g' /etc/default/portsentry
+	
+	sed -i  's/^\([^#].*\)/#\1/' /etc/portsentry/portsentry.ignore.static
+	
+	sed -i 's/\(BLOCK_TCP=\).*/\1"1"/g' /etc/portsentry/portsentry.conf
+	sed -i 's/\(BLOCK_UDP=\).*/\1"1"/g' /etc/portsentry/portsentry.conf
+	sed -i 's|^\(KILL_ROUTE="\).*$|\1/usr/sbin/nft add element filter blackhole { $TARGET$ }"|' /etc/portsentry/portsentry.conf
+
+	systemctl start portsentry
+	systemctl enable portsentry
+}
+
+rkhunter () {
+	apt-get install -f rkhunter libwww-perl
+	
+	#vim /etc/rkhunter.conf
+	# test conf
+	rkhunter -c --sk
+	# update
+	rkhunter --propupd
+	echo 'DPkg::Post-Invoke { "if [ -x /usr/bin/rkhunter ]; then /usr/bin/rkhunter --propupd; fi"; };' > /etc/apt/apt.conf.d/90rkhunter
+}
+
+disable_ipv6 () {
+	#disable ipv6 support
+	sed -i 's|\(GRUB_CMDLINE_LINUX=\)""|\1"ipv6.disable=1"|' /etc/default/grub
+	update-grub2
+}
+
+hardening_srv () {
+	disable_ipv6
+	fail2ban
+	portsentry
+	#rkhunter
+}
+
+hardening_srv
