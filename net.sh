01cff8067d0d62f7a7f7541c51599c5801e39e8f
peondusud <reflexion_31@hotmail.com>
2016-10-22 18:48:07 +0200

    Update net.sh

--- net.sh
+++ net.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 DIR=/tmp/dedi.serv
 
-
 rm -rf  /tmp/dedi.serv || true
 apt-get update 
 apt-get upgrade
 apt-get install -y git 
 git clone https://github.com/peondusud/dedi.serv.git $DIR
+$DIR/debian.sh
