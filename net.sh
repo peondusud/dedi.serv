5498a9a9a7354c710d885bb84933ec97bf1f99bb
peondusud <reflexion_31@hotmail.com>
2016-10-29 16:38:31 +0200

    Update net.sh

--- net.sh
+++ net.sh
@@ -6,6 +6,6 @@ set -x
 rm -rf  /tmp/dedi.serv || true
 apt-get update || true
 apt-get upgrade -y || true
-apt-get install -y git || true
+apt-get install -y git apache2-utils || true
 git clone https://github.com/peondusud/dedi.serv.git $DIR
 source $DIR/debian.sh
